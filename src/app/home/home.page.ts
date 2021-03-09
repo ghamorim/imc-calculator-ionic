20b9bd78145cfba0da4deb923c2e4c27e1f90c58
ghamorim <gabriel.amorim@estudante.ufla.br>
2021-03-08 23:54:38 -0300

    :rocket: Adiciona classificação IMC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,7 +11,7 @@ export class HomePage {
   weight: number;
   height: number;
 
-  constructor(private toastController: ToastController) {}
+  constructor(private toastController: ToastController) { }
 
   isFormValid() {
     return (this.height && this.weight && this.height > 0 && this.weight > 0);
@@ -19,7 +19,21 @@ export class HomePage {
 
   onCalculate() {
     const imc = this.weight / (this.height * this.height);
-    this.showMessage(`IMC = ${imc.toFixed(2)}`);
+    let result_msg = `IMC = ${imc.toFixed(2)}`
+
+    if (imc < 18.5) {
+      result_msg += ' - Magreza';
+    } else if (imc >= 18.5 && imc < 25) {
+      result_msg += ' - Normal';
+    } else if (imc >= 25 && imc < 30) {
+      result_msg += ' - Sobrepeso';
+    } else if (imc >= 30 && imc < 40) {
+      result_msg += ' - Obesidade';
+    } else {
+      result_msg += ' - Obesidade Grave';
+    }
+
+    this.showMessage(result_msg);
   }
 
   async showMessage(msg: string) {
